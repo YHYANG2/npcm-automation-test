5fb0310e64188960b1ce8c282bffab1ed22217e6
Eason Yang <j2anfernee@gmail.com>
2025-03-11 17:07:49 +0800

    pwm test
    Signed-off-by: Eason Yang <j2anfernee@gmail.com>

--- data/arbel-evb/pwm_fan_test.sh
+++ data/arbel-evb/pwm_fan_test.sh
@@ -7,21 +7,21 @@ echo 0 > /sys/class/hwmon/hwmon1/pwm1
 sleep 5
 fanin=`cat /sys/class/hwmon/hwmon1/fan1_input`
 echo "Fanin: $fanin"
-test $fanin -lt 1100 -a $fanin -gt 900
+test $fanin -lt 2200 -a $fanin -gt 900
 
 echo "Set pwm to 125"
 echo 125 > /sys/class/hwmon/hwmon1/pwm1
 sleep 5
 fanin=`cat /sys/class/hwmon/hwmon1/fan1_input`
 echo "Fanin: $fanin"
-test $fanin -lt 1450 -a $fanin -gt 1250
+test $fanin -lt 2200 -a $fanin -gt 1250
 
 echo "Set pwm to 255"
 echo 255 > /sys/class/hwmon/hwmon1/pwm1
 sleep 5
 fanin=`cat /sys/class/hwmon/hwmon1/fan1_input`
 echo "Fanin: $fanin"
-test $fanin -lt 2000 -a $fanin -gt 1800
+test $fanin -lt 2200 -a $fanin -gt 1800
 
 systemctl start phosphor-pid-control.service
 
