eaa376b2bf72654d7d5bdf4ba24ce06fec4373d3
Eason Yang <j2anfernee@gmail.com>
2025-04-01 09:52:09 +0800

    arbel-evb: update test script for I3C
    Signed-off-by: Eason Yang <j2anfernee@gmail.com>

--- data/arbel-evb/i3c_test.sh
+++ data/arbel-evb/i3c_test.sh
@@ -4,10 +4,10 @@ log_file="/tmp/log/i3c_test.log"
 
 # attach OpenBIC to i3c bus#0
 
-echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/unbind
-sleep 1
-echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/bind
-sleep 1
+#echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/unbind
+#sleep 1
+#echo "fff11000.i3c" > /sys/bus/platform/drivers/silvaco-i3c-master/bind
+#sleep 1
 
 echo "check I3C pid sysfs exist" > $log_file
 pid=`cat /sys/bus/i3c/devices/0-63212345607/pid`
